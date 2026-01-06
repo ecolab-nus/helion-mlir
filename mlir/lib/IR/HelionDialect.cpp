873fc9c479520742286be23f734fed50b1a64049
zhenyu bai <zhenyu.bai@nus.edu.sg>
2026-01-06 10:45:36 +0000

    add the missing files

---
+++ mlir/lib/IR/HelionDialect.cpp
@@ -0,0 +1,20 @@
+//===- HelionDialect.cpp ----------------------------------------*- C++ -*-===//
+//
+// Part of the Helion FX → MLIR prototype.
+//
+//===----------------------------------------------------------------------===//
+
+#include "helion/IR/HelionDialect.h"
+#include "helion/IR/HelionOps.h"
+
+using namespace mlir;
+using namespace helion;
+
+#include "HelionDialect.cpp.inc"
+
+void HelionDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "HelionOps.h.inc"
+      >();
+}
---
+++ mlir/lib/IR/HelionOps.cpp
@@ -0,0 +1,13 @@
+//===- HelionOps.cpp ------------------------------------------*- C++ -*-===//
+//
+// Part of the Helion FX → MLIR prototype.
+//
+//===----------------------------------------------------------------------===//
+
+#include "helion/IR/HelionOps.h"
+
+using namespace mlir;
+using namespace helion;
+
+#define GET_OP_CLASSES
+#include "HelionOps.cpp.inc"
